e810393c5968134a2a1d448d000ce6b2289f3974
realgoodegg <tomisalive@gmail.com>
2023-04-29 14:28:24 +0100

    file selection functionality implemented, select all and clear selected buttons add to GUI

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+another checksum application (aca)
+author: Thomas Luke Ruane
+github repo: https://github.com/realgoodegg/another-checksum-application
+last modified: 2023-04-29
+"""
+
 import wx
 from checksum_service import FileHashingService
 from concurrent import futures
@@ -17,6 +24,7 @@ class MainUIFrame(wx.Frame):
         self.green = "○"
         self.orange = "-"
         self.red = "╳"
+        self.selected_items = []  # List of selected items in the source_list
 
         super().__init__(parent=None, title="aca", size=(530, 600))
         self.SetSizeHints(410, 600, -1, -1)
@@ -30,6 +38,13 @@ class MainUIFrame(wx.Frame):
         self.set_source_button.Bind(wx.EVT_BUTTON, self.on_button_press)
         self.refresh_source_button.Bind(wx.EVT_BUTTON, self.on_button_press)
         self.refresh_source_button.Enable(False)
+
+        self.select_button = wx.Button(panel, label="Select All")
+        self.clear_button = wx.Button(panel, label="Clear Selected")
+        self.select_button.Bind(wx.EVT_BUTTON, self.on_button_press)
+        self.clear_button.Bind(wx.EVT_BUTTON, self.on_button_press)
+        self.select_button.Enable(False)
+        self.clear_button.Enable(False)
 
         self.source_list = wx.ListCtrl(
             panel,
@@ -45,6 +60,8 @@ class MainUIFrame(wx.Frame):
         self.source_list.SetColumnWidth(2, 40)
 
         self.source_list.Bind(wx.EVT_SIZE, self.on_size)
+        self.source_list.Bind(wx.EVT_LIST_ITEM_SELECTED, self.on_item_selected)
+        self.source_list.Bind(wx.EVT_LIST_ITEM_DESELECTED, self.on_item_deselected)
 
         self.generate_button = wx.Button(panel, label="Generate")
         self.generate_button.Bind(wx.EVT_BUTTON, self.on_button_press)
@@ -61,16 +78,25 @@ class MainUIFrame(wx.Frame):
 
         self.source_layout = wx.BoxSizer(wx.HORIZONTAL)
         self.source_layout.Add(
-            self.source_location, 2, wx.LEFT | wx.RIGHT | wx.EXPAND, 2
+            self.source_location, 1, wx.LEFT | wx.RIGHT | wx.EXPAND, 2
         )
         self.source_layout.Add(
             self.refresh_source_button, 0, wx.LEFT | wx.RIGHT | wx.EXPAND, 1
+        )
+
+        self.selection_layout = wx.BoxSizer(wx.HORIZONTAL)
+        self.selection_layout.Add(
+            self.select_button, 1, wx.LEFT | wx.RIGHT | wx.EXPAND, 2
+        )
+        self.selection_layout.Add(
+            self.clear_button, 0, wx.LEFT | wx.RIGHT | wx.EXPAND, 2
         )
 
         # Main vertical layout for the UI elements
         self.vertical_layout = wx.BoxSizer(wx.VERTICAL)
         self.vertical_layout.Add(self.set_source_button, 0, wx.ALL | wx.EXPAND, 5)
         self.vertical_layout.Add(self.source_layout, 0, wx.ALL | wx.EXPAND, 5)
+        self.vertical_layout.Add(self.selection_layout, 1, wx.ALL | wx.EXPAND, 5)
         self.vertical_layout.Add(self.source_list, 1, wx.ALL | wx.EXPAND, 5)
         self.vertical_layout.Add(self.generate_button, 0, wx.ALL | wx.EXPAND, 5)
         self.vertical_layout.Add(self.progress_bar, 0, wx.ALL | wx.EXPAND, 8)
@@ -81,11 +107,11 @@ class MainUIFrame(wx.Frame):
         # Resize the columns in ListCtrl to fit the window
         width = (
             self.source_list.GetClientSize().GetWidth() - 40
-        )  # -40 retain status light column
+        )  # -40 retain status symbol column
 
         columns = (
             self.source_list.GetColumnCount() - 1
-        )  # -1 excludes status light column from resize
+        )  # -1 excludes status symbol column from resize
         column_width = width // columns
         for i in range(columns):
             self.source_list.SetColumnWidth(i, column_width)
@@ -109,8 +135,8 @@ class MainUIFrame(wx.Frame):
         # Get the file list from the file hashing service and populate the list view
         self.fhs.get_file_list()
         self.populate_list_view()
-        self.generate_button.Enable(True)
         self.refresh_source_button.Enable(True)
+        self.select_button.Enable(True)
 
     def set_item_labels(self, index, data):
         # Set the labels for the list view items
@@ -144,6 +170,23 @@ class MainUIFrame(wx.Frame):
             f"{total_files} files found, {with_hash} files with checksums"
         )
 
+    def on_item_selected(self, event):
+        index = event.GetIndex()
+        if index not in self.selected_items:
+            self.selected_items.append(index)
+
+        self.clear_button.Enable(True)
+        self.generate_button.Enable(True)
+
+    def on_item_deselected(self, event):
+        index = event.GetIndex()
+        if index in self.selected_items:
+            self.selected_items.remove(index)
+
+        if len(self.selected_items) == 0:
+            self.clear_button.Enable(False)
+            self.generate_button.Enable(False)
+
     def insert_list_view(self, file_index, file_data):
         # Insert a new item into the list view
         self.source_list.DeleteItem(file_index)
@@ -155,36 +198,38 @@ class MainUIFrame(wx.Frame):
         # Update the status light column colour
         self.source_list.SetItem(file_index, column=2, label=status)
 
-    def update_progress_bar(self, file_index):
+    def update_progress_bar(self, max_value, file_index):
         # Update the progress bar and status bar with the current file being processed
-        current_item = int(file_index + 1)
-        max_value = len(self.fhs.file_data_list)
-        percent = int((current_item / max_value) * 100)
-        self.progress_bar.SetValue(percent)
+        self.progress_bar.Range = max_value
+        for i in range(max_value):
+            current_item = i + 1
+            percent = int((current_item / max_value) * 100)
+            self.progress_bar.SetValue(percent)
         self.status_report.SetStatusText(
             f"{current_item} of {max_value} files processed"
         )
         if current_item == max_value:
-            self.status_report.SetStatusText("All files processed")
             self.set_source_button.Enable(True)
             self.refresh_source_button.Enable(True)
 
-    def on_generate_complete(self, file_index, file_data):
+    def on_generate_complete(self, max_value, file_index, file_data):
         # Run file hashing service adn call list view update and progress bar update when complete
         self.set_source_button.Enable(False)
         self.refresh_source_button.Enable(False)
+        self.select_button.Enable(False)
+        self.clear_button.Enable(False)
         self.generate_button.Enable(False)
+
         if file_data["hash"] == "…":
             self.fhs.generate_hash(file_data)
             wx.CallAfter(self.insert_list_view, file_index, file_data)
             wx.CallAfter(self.update_status_colour, file_index, self.green)
-            wx.CallAfter(self.update_progress_bar, file_index)
+            wx.CallAfter(self.update_progress_bar, max_value, file_index)
         else:
-            wx.CallAfter(self.update_progress_bar, file_index)
+            wx.CallAfter(self.update_progress_bar, max_value, file_index)
             wx.CallAfter(self.update_status_colour, file_index, self.orange)
-        # time.sleep(
-        #     0.1
-        # )  # Delay to allow the UI to update before the next file is processed
+
+        self.selected_items.clear()
 
     def on_button_press(self, event):
         # Handle button presses
@@ -200,13 +245,30 @@ class MainUIFrame(wx.Frame):
             self.progress_bar.SetValue(0)
             self.fhs.get_file_list()
             self.populate_list_view()
+            self.select_button.Enable(True)
+
+        elif button_label == "Select All":
+            for file_index in range(self.source_list.GetItemCount()):
+                self.source_list.Select(file_index)
+
+        elif button_label == "Clear Selected":
+            self.selected_items.clear()
+            self.source_list.SetItemState(-1, 0, wx.LIST_STATE_SELECTED)
+            self.clear_button.Enable(False)
+            self.generate_button.Enable(False)
 
         elif button_label == "Generate":
             self.status_report.SetStatusText("Generating checksums...")
-            for file_index, file_data in enumerate(self.fhs.file_data_list):
-                thread_pool_executor.submit(
-                    self.on_generate_complete, file_index, file_data
-                )  # Run the file hashing service in a thread pool
+            if len(self.selected_items) > 0:
+                max_value = len(self.selected_items)
+                print(max_value)
+                for index in self.selected_items:
+                    file_data = self.fhs.file_data_list[index]
+                    thread_pool_executor.submit(
+                        self.on_generate_complete, max_value, index, file_data
+                    )
+            else:
+                pass
 
 
 if __name__ == "__main__":
